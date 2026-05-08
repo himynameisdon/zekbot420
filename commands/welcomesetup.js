9e1fb8411d4d5cb90d7bbbd1b8256ea1d088c494
don <zekkiewashere@icloud.com>
2026-05-08 10:48:33 +0200

    NOBODY TOLD ME THE WELCOMING SYSTEM WAS BROKEN WTF

--- commands/welcomesetup.js
+++ commands/welcomesetup.js
@@ -1,24 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-const configPath = path.join(__dirname, '../welcomeConfig.json');
 
 module.exports = {
   name: 'welcomesetup',
   aliases: ['ws'],
   async execute(message, args) {
-    if (!message.member.permissions.has('Administrator')) return;
+    if (!message.member.permissions.has('Administrator')) return message.reply('You need Administrator permissions.');
 
     const channel = message.mentions.channels.first();
-    if (!channel) return message.reply('Mention the channel to set for welcoming.');
+    if (!channel) return message.reply('Usage: ,welcomesetup #channel');
 
-    let config = {};
-    if (fs.existsSync(configPath)) {
-      config = JSON.parse(fs.readFileSync(configPath));
+    const guildDir = path.join(__dirname, '../data', message.guild.id);
+    const configPath = path.join(guildDir, 'welcomeConfig.json');
+
+    try {
+      fs.mkdirSync(guildDir, { recursive: true });
+      console.log('[welcomesetup] saving to:', configPath);
+      fs.writeFileSync(configPath, JSON.stringify({ channelId: channel.id }, null, 2));
+      message.reply(`Welcome channel set to ${channel}.`);
+    } catch (err) {
+      console.error('[welcomesetup]', err);
+      message.reply('Failed to save config: ' + err.message);
     }
-
-    config[message.guild.id] = channel.id;
-    fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
-
-    message.reply(`Welcome channel has been set to ${channel}.`);
   }
 };
