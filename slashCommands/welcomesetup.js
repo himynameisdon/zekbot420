049b48626425c78be1d780fd7c97df450d8de2a8
don <zekkiewashere@icloud.com>
2026-05-08 10:53:35 +0200

    Forgot to make slash command ver

---
+++ slashCommands/welcomesetup.js
@@ -0,0 +1,47 @@
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    PermissionsBitField,
+    InteractionContextType,
+    ChannelType
+} = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('welcomesetup')
+        .setDescription('Set the welcome channel for this server')
+        .setContexts(InteractionContextType.Guild)
+        .setIntegrationTypes(0)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .addChannelOption((opt) =>
+            opt
+                .setName('channel')
+                .setDescription('Channel to send welcome messages in')
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
+        ),
+
+    async execute(interaction) {
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+            return interaction.reply({
+                content: 'You need the Manage Server permission to use this command.',
+                ephemeral: true
+            });
+        }
+
+        const channel = interaction.options.getChannel('channel');
+        const guildDir = path.join(__dirname, '../../data', interaction.guild.id);
+        const configPath = path.join(guildDir, 'welcomeConfig.json');
+
+        try {
+            fs.mkdirSync(guildDir, { recursive: true });
+            fs.writeFileSync(configPath, JSON.stringify({ channelId: channel.id }, null, 2));
+            return interaction.reply({ content: `Welcome channel set to ${channel}.`, ephemeral: true });
+        } catch (err) {
+            console.error('[welcomesetup]', err);
+            return interaction.reply({ content: 'Failed to save config: ' + err.message, ephemeral: true });
+        }
+    }
+};
